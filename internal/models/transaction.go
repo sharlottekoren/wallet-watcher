8a1e5c565fd031bbfcb5e4eb444bd5cb38668289
sharlottekoren <sharlottekoren@gmail.com>
2026-05-30 22:56:46 +0100

    undo everything

--- internal/models/transaction.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Transaction struct {
-	ID          string  `json:"id"`
-	Amount      float64 `json:"amount"`
-	Category    string  `json:"category"`
-	Description string  `json:"description"`
-}
